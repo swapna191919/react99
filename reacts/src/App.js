c764b9d4157f04afbe775a3df0eacea1d980bb5e
swapna191919 <swapnajikkula@gmail.com>
2024-03-11 15:52:58 +0530

    added

---
+++ reacts/src/App.js
@@ -0,0 +1,11 @@
+// import logo from './logo.svg';
+import './App.css';
+import Abc from './components/thulasi';
+
+function App() {
+  return (
+   <Abc></Abc>
+  );
+}
+
+export default App;
---
+++ reacts/src/components/thulasi.js
@@ -0,0 +1,8 @@
+function Abc(){
+    return(
+        <>
+        <h1>hello world</h1>
+        </>
+    )
+}
+export default Abc;
